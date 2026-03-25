bac4c7b9d2c68fea5de77b07e6307704bb6cf589
1shrikantsc-spc <1shrikant.sc@gmail.com>
2026-03-25 14:51:05 +0530

    Change API destination to use BACKEND_URL variable
    Updated the API destination URL to use an environment variable.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,23 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "lh3.googleusercontent.com",
-            },
-        ],
-    },
-
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: "https://kernels-slap-backend.onrender.com/api/:path*",
-            },
-        ];
-    }, 
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.BACKEND_URL || "http://127.0.0.1:8000"}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
